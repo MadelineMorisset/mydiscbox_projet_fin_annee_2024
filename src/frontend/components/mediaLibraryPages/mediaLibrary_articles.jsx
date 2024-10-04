54460c84c6c19e434b86d2b7495f8683259687b4
Madeline Ricateau <madeline.ricateau@outlook.fr>
2024-10-04 09:28:56 +0200

    V1 - Created the page "mediaLibrary_articles" to check the connection or send to the correct pages

---
+++ src/frontend/components/mediaLibraryPages/mediaLibrary_articles.jsx
@@ -0,0 +1,12 @@
+function MediaLibraryArticles() {
+    return (
+        <>
+            <div>
+                <h1>Médiathèque</h1>
+                <p>Cette page est vide pour l&quot;instant.</p>
+            </div>
+        </>
+    );
+}
+
+export default MediaLibraryArticles;
