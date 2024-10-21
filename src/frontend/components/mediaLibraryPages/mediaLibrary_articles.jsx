68663a301c0de4c368dac2d06007dd3a474b9c78
Madeline Ricateau <madeline.morisset@emilfrey.fr>
2024-10-21 15:01:33 +0200

    V1 - Modification médiathèque

--- src/frontend/components/mediaLibraryPages/mediaLibrary_articles.jsx
+++ src/frontend/components/mediaLibraryPages/mediaLibrary_articles.jsx
@@ -1,10 +1,75 @@
+// React imports
+import { Link } from "react-router-dom";
+// Images
+import Logo from "../../assets/images/darkLogo.png";
+// CSS
+import "../../styles/mediaLibrary_mobile.css";
+
 function MediaLibraryArticles() {
     return (
         <>
-            <div>
-                <h1>Médiathèque</h1>
-                <p>Cette page est vide pour l&quot;instant.</p>
-            </div>
+            <header>
+                <Link to="/home" id="mediaLibrary_logoContainer">
+                    <img
+                        src={Logo}
+                        alt="Logo de myDiscBox"
+                        width={50}
+                        height={50}
+                        className="logPage_logo"
+                    />
+                </Link>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/home/articles">Articles</a>
+                        </li>
+                        <li>
+                            <a
+                                href="/home/shelves"
+                                hidden
+                                alt="À ajouter en V4"
+                            >
+                                Étagères
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/home/genres" hidden alt="À ajouter en V3">
+                                Genres
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
+            <main></main>
+
+            <footer>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/home">Médiathèque</a>
+                        </li>
+                        <li>
+                            <a
+                                href="/home/add-scann"
+                                hidden
+                                alt="À ajouter en V4"
+                            >
+                                Ajout article
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="/home/account"
+                                hidden
+                                alt="À ajouter en V3"
+                            >
+                                Compte
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </footer>
         </>
     );
 }
